898305f76e2c0d7f8b5e616c391a73b6da8f428d
esquith <esquithallen@gmail.com>
2023-05-11 11:18:32 -0400

    Create packs folder and application.js file

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,9 @@
+// Import your JavaScript dependencies or modules here
+// For example:
+// import 'bootstrap';
+// import './custom-script';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Your JavaScript code goes here
+  console.log('Hello, webpacker!');
+});
